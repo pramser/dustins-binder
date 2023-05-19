cad827969688202cb7c60ce21d6a8bdc73ea02cd
Patrick Ramser <pramser@gmail.com>
2023-05-19 17:14:27 -0400

    Dark background, even margins

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,11 @@ import { ALBUMS } from "@/types"
 
 export default function Home() {
   return (
-    <main className="flex flex-row p-8">
+    <main className="flex flex-row mt-4 mx-4">
       {ALBUMS.map((album) => (
         <Image
           key={album.name}
-          className="m-2"
+          className="m-4"
           src={album.image}
           alt={album.name}
           width={250}
