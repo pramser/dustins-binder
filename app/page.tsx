7fa1deab0bc490d29b0eb60fe599b0357eca93d5
Patrick Ramser <pramser@gmail.com>
2023-06-20 12:10:41 -0400

    Fill content on page and scale list to different resolutions

--- app/page.tsx
+++ app/page.tsx
@@ -6,17 +6,21 @@ import { ALBUMS } from "@/types"
 
 export default function Home() {
   return (
-    <main className="flex flex-row flex-wrap justify-center mt-4 mx-4">
+    <main className="flex flex-row flex-wrap">
       {ALBUMS.map((album) => (
-        <Image
-          key={album.name}
-          className="m-4 border border-black shadow-md cursor-pointer hover:blur-sm"
-          src={album.image}
-          blurDataURL="/placeholder.png"
-          alt={album.name}
-          width={220}
-          height={220}
-        />
+        <div className="w-fill h-fit md:w-1/4 md:h-1/4 lg:w-1/6 lg:h-1/6 relative">
+          <Image
+            key={album.name}
+            className="cursor-pointer hover:blur-sm object-cover"
+            src={album.image}
+            blurDataURL="/placeholder.png"
+            alt={album.name}
+            height={0}
+            width={0}
+            sizes="100vw"
+            style={{ width: "100%", height: "auto" }}
+          />
+        </div>
       ))}
     </main>
   )
