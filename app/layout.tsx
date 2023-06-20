a9603e3aca68b38a971f01ae306984f5cb33dc62
Patrick Ramser <pramser@gmail.com>
2023-06-20 12:36:43 -0400

    Add header I'll later use to sort the albums in the list

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,10 @@
 import "./globals.css"
+
+// next
 import { Inter } from "next/font/google"
+
+// hero-icons
+import { FunnelIcon } from "@heroicons/react/24/solid"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -15,7 +20,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-neutral-800`}>{children}</body>
+      <body className={`${inter.className} bg-neutral-800`}>
+        <header className="bg-gradient-to-r from-blue-700 to-indigo-900 flex flex-row px-4 py-2 items-center text-white w-fill">
+          <FunnelIcon className="h-4 w-4" />
+          &nbsp;
+          <span className="cursor-default">album_a_z</span>
+          <button className="bg-indigo-900 hover:bg-blue-700 border border-white rounded-md ml-4 px-2 py-1">
+            sort
+          </button>
+        </header>
+        {children}
+      </body>
     </html>
   )
 }
