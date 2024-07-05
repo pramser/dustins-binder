9260c48d1c25a4e24c8095ad96a47087a72e16b2
Patrick Ramser <pramser@gmail.com>
2024-07-05 12:02:39 -0400

    Fill screen with album art when smaller device is used

--- components/albumCover.tsx
+++ components/albumCover.tsx
@@ -9,7 +9,7 @@ interface Props {
 
 export default function AlbumCover({ album, setSelectedAlbum }: Props) {
   return (
-    <div key={album.name} className="w-fill h-fit md:w-1/4 md:h-1/4 lg:w-1/6 lg:h-1/6 relative">
+    <div key={album.name} className="w-full h-fit md:w-1/4 md:h-1/4 lg:w-1/6 lg:h-1/6 relative">
       <Image
         className="cursor-pointer object-cover z-0"
         src={album.image}
@@ -25,13 +25,11 @@ export default function AlbumCover({ album, setSelectedAlbum }: Props) {
         className="w-full h-full bg-white/75 duration-300 inset-0 opacity-0 hover:opacity-100 absolute z-10 flex flex-col justify-center items-center"
         onClick={() => setSelectedAlbum(album)}
       >
-        <p className="italic max-w-64 lg:max-w-48 text-lg text-center text-ellipsis truncate whitespace-nowrap">
+        <p className="italic max-w-full text-lg text-center text-ellipsis truncate whitespace-nowrap">
           &quot;{album.name}&quot;
         </p>
-        <p className="font-bold max-w-64 lg:max-w-48 text-xl text-center text-ellipsis truncate whitespace-nowrap">
-          {album.artist}
-        </p>
-        <p className="max-w-64 lg:max-w-48 text-sm text-center text-ellipsis truncate whitespace-nowrap">
+        <p className="font-bold max-w-full text-xl text-center text-ellipsis truncate whitespace-nowrap">{album.artist}</p>
+        <p className="max-w-full text-sm text-center text-ellipsis truncate whitespace-nowrap">
           {album.releaseDate.getFullYear()}
         </p>
       </div>
